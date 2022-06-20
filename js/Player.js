eb39615f86ab9efbe70853dc1c8557515ba47185
edwedja <edwedja_lima@hotmail.com>
2022-06-20 12:47:03 -0300

    add players

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,48 @@
 class Player {
-  constructor() {}
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index; // cria o index de cada jogador
+
+    if (this.index === 1) { // posição de cada jogador na pista
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({ // atualiza no banco de dados
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY
+    });
+  }
+
+  getCount() { // pega a informaçao da quantidade de players do banco de dados
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) { // atualiza sempre no banco de dados(escreve)
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+ 
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+      ///console.log(allPlayers)
+    });
+   
+  }
+
+
 }
--- sketch.js
+++ sketch.js
@@ -1,24 +1,37 @@
 var canvas;
-var backgroundImage;
+var backgroundImage, car1_img, car2_img, track;
 var bgImg;
-var database;
+var database, gameState;
 var form, player;
 var playerCount;
+var allPlayers, car1, car2;
+var cars = [];
 
 function preload() {
   backgroundImage = loadImage("./assets/planodefundo.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/pista.png");
 }
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  //database = firebase.database();
+  database = firebase.database();
   game = new Game();
+  game.getState(); // pega o valor do estado do jogo!
   game.start();
 
 }
 
 function draw() {
   background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1); 
+  }
+
+  if (gameState === 1) {
+    game.play(); 
+  }
 }
 
 function windowResized() {
