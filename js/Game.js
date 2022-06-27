5e6b710905c4ea72560b04be4bc40cb1d81760ca
edwedja <edwedja_lima@hotmail.com>
2022-06-27 12:07:22 -0300

    placar

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,14 @@
 class Game {
-  constructor() {}
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+
+    this.leadeboardTitle = createElement("h2"); //Titulo do quadro de placar
+
+    this.leader1 = createElement("h2"); // condutor 1
+    this.leader2 = createElement("h2"); // condutor 2
+  }
+
   getState() {  // 1º
     var gameStateRef = database.ref("gameState");
     gameStateRef.on("value", function(data) {
@@ -34,35 +43,103 @@ class Game {
     form.hide(); // paga o form
     form.titleImg.position(40, 50); // redefine o titulo
     form.titleImg.class("gameTitleAfterEffect"); // novo estilo do titulo
+
+    this.resetTitle.html("Reiniciar");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width/2+200,50);
+  
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 230, 100);
+
+    this.leadeboardTitle.html("Placar");
+    this.leadeboardTitle.class("resetText");
+    this.leadeboardTitle.position(width / 3 - 60, 40);
+
+    this.leader1.class("leadersText");
+    this.leader1.position(width / 3 - 50, 80);
+
+    this.leader2.class("leadersText");
+    this.leader2.position(width / 3 - 50, 130);
   }
 
   play(){
-   this.handleElements()
+   this.handleElements();
 
    Player.getPlayersInfo(); // o temto todo atualizando a informação dos players 
-  if (allPlayers !== undefined) {  
-   //if(playerCount === 2){
-    image (track, 0, -height *5, width, height *6)
-    var index = 0
+   if (allPlayers !== undefined) {  
+    image (track, 0, -height *5, width, height *6);
+
+    var index = 0;
     for(var plr in allPlayers){
-    index++
-    var x = allPlayers[plr].positionX
-    var y = height - allPlayers[plr].positionY
-    cars[index-1].position.x = x
-    cars[index-1].position.y = y
-    if(index === player.index){
-      camera.position.y = cars[index-1].position.y
+      index++;
+      var x = allPlayers[plr].positionX;
+      var y = height - allPlayers[plr].positionY;
+      cars[index-1].position.x = x;
+      cars[index-1].position.y = y;
+
+      if(index === player.index){
+        stroke(10);
+        fill("red");
+        ellipse(x, y, 60, 60);
+
+        camera.position.y = cars[index-1].position.y;
+      }
     }
-    }
-    this.Setinha()
-    drawSprites()
+
+    this.Setinha();
+    this.showLeaderboard();
+    drawSprites();
    }
   }
+
+  showLeaderboard() {
+    var leader1, leader2;
+    var players = Object.values(allPlayers);
+    if (
+      (players[0].rank === 0 && players[1].rank === 0) ||
+      players[0].rank === 1
+    ) {
+      // &emsp;    Essa etiqueta é usada para exibir quatro espaços.
+      leader1 =
+        players[0].rank +
+        "&emsp;"  +
+        players[0].name +
+        "&emsp;" +
+        players[0].score;
+
+      leader2 =
+        players[1].rank +
+        "&emsp;" +
+        players[1].name +
+       "&emsp;" +
+        players[1].score;
+    }
+
+    if (players[1].rank === 1) {
+      leader1 =
+        players[1].rank +
+        "&emsp;" +
+        players[1].name +
+        "&emsp;" +
+        players[1].score;
+
+      leader2 =
+        players[0].rank +
+        "&emsp;" +
+        players[0].name +
+        "&emsp;" +
+        players[0].score;
+    }
+
+    this.leader1.html(leader1);
+    this.leader2.html(leader2);
+  }
+
   
   Setinha(){
     if(keyIsDown(UP_ARROW)){
-      player.positionY += 10
-      player.update()
+      player.positionY += 10;
+      player.update();
     }
   }
 }
