2cd39be69cfc157e34f7815efe75dee4b2469ba3
edwedja <edwedja_lima@hotmail.com>
2022-06-28 01:06:44 -0300

    update

--- sketch.js
+++ sketch.js
@@ -6,12 +6,19 @@ var form, player;
 var playerCount;
 var allPlayers, car1, car2;
 var cars = [];
+var fuelImage, powerCoinImage, obstacle1Image,obstacle2Image;
+var fuels,powerCoins;
 
 function preload() {
   backgroundImage = loadImage("assets/planodefundo.png");
   car1_img = loadImage("assets/car1.png");
   car2_img = loadImage("assets/car2.png");
   track = loadImage("assets/PISTA.png");
+
+  fuelImage = loadImage("assets/fuel.png");
+  powerCoinImage = loadImage("assets/goldCoin.png");
+  obstacle1Image = loadImage("assets/obstacle1.png");
+  obstacle2Image = loadImage("assets/obstacle2.png");
 }
 
 function setup() {
